c1366ce0a0b74661d38fbd091106b2983b678930
Trương Đình Minh Tuệ <tuetruong1889@gmail.com>
2024-04-04 13:32:45 +0700

    [feat] connect db

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { User } from "@prisma/client";
+import { PrismaService } from "./prisma.service";
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
   getHello(): string {
-    return 'Xin chào anh Bình Minh';
+    return "Xin chào anh Bình Minh";
+  }
+
+  async user(id: number): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    // return this.prisma.$queryRaw`select * from User where 'id' = ${id}`;
   }
 }
